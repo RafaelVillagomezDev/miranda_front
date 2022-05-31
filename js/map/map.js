493a1dd89bf892f8f575f4d439c0abcb4d0848e8
RafaelVillagomezDev <rafaelvillagomez.dev@gmail.com>
2022-05-31 14:03:01 +0200

    Creación DistnaceMatrix

--- js/map/map.js
+++ js/map/map.js
@@ -70,6 +70,76 @@ function initMap() {
       handleLocationError(false, infoWindow, map.getCenter())
     }
   })
+
+  const geocoder = new google.maps.Geocoder()
+  const service = new google.maps.DistanceMatrixService()
+
+  // build request
+  const origin1 = { lat: 55.93, lng: -3.118 }
+  const origin2 = 'Greenwich, England'
+  const destinationA = 'Stockholm, Sweden'
+  const destinationB = { lat: 50.087, lng: 14.421 }
+  const request = {
+    origins: [origin1, origin2],
+    destinations: [destinationA, destinationB],
+    travelMode: google.maps.TravelMode.DRIVING,
+    unitSystem: google.maps.UnitSystem.METRIC,
+    avoidHighways: false,
+    avoidTolls: false,
+  }
+
+  // put request on page
+  document.getElementById('request').innerText = JSON.stringify(
+    request,
+    null,
+    2,
+  )
+
+  // get distance matrix response
+  service.getDistanceMatrix(request).then((response) => {
+    // put response
+    console.log(response)
+    document.getElementById('response').innerText = JSON.stringify(
+      response,
+      null,
+      2,
+    )
+
+    // show on map
+    const originList = response.originAddresses
+    const destinationList = response.destinationAddresses
+
+    deleteMarkers(markersArray)
+
+    const showGeocodedAddressOnMap = (asDestination = boolean) => {
+      const handler = ({ results } = google.maps.GeocoderResponse) => {
+        map.fitBounds(bounds.extend(results[0].geometry.location))
+        markersArray.push(
+          new google.maps.Marker({
+            map,
+            position: results[0].geometry.location,
+            label: asDestination ? 'D' : 'O',
+          }),
+        )
+      }
+
+      return handler
+    }
+
+    for (let i = 0; i < originList.length; i++) {
+      const results = response.rows[i].elements
+
+      geocoder
+        .geocode({ address: originList[i] })
+        .then(showGeocodedAddressOnMap(false))
+
+      for (let j = 0; j < results.length; j++) {
+        geocoder
+          .geocode({ address: destinationList[j] })
+          .then(showGeocodedAddressOnMap(true))
+      }
+    }
+  })
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -81,10 +151,22 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   )
   infoWindow.open(map)
 }
+// //--------------DISTANCE MATRIX API--------------------------
+
+// // initialize services
+//
+
+function deleteMarkers(markersArray = google.maps.Marker) {
+  for (let i = 0; i < markersArray.length; i++) {
+    markersArray[i].setMap(null)
+  }
+
+  markersArray = []
+}
 
 const locations = [
   {
-    name: 'Holtel Miranda Sevilla La Nueva',
+    name: 'Hotel Miranda Sevilla La Nueva',
     lat: 40.348094,
     lng: -4.027125,
   },
