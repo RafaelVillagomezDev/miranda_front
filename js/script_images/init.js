97d4947d72f395f7e9021a5607f08283c7f75cc8
RafaelVillagomezDev <rafaelvillagomez.dev@gmail.com>
2022-11-20 11:48:00 +0100

    Creacion menu hamburguesa

--- js/script_images/init.js
+++ js/script_images/init.js
@@ -3,24 +3,22 @@
 //Metodo abrir hamburguesa//
 
 const open = () => {
-  document
-    .querySelector('#section_burguer')
-    .classList.toggle('menu_hamburguesa')
-  if (document.getElementsByClassName('menu_hamburguesa').length > 0) {
-    document.querySelector('#btn_close').classList.toggle('btn_close')
-  } else {
-    document.getElementById('btn_close').style.display = 'none'
-  }
+  hamburguesa.setAttribute('style', 'display:block')
+  btn_close.setAttribute('style', 'display:block')
+  section_main.classList.add('main_container')
 }
 
 const close = () => {
-  btn_close.classList.toggle('menu_hamburguesa')
+  if (hamburguesa) {
+    hamburguesa.setAttribute('style', 'display:none')
+    section_main.classList.remove('main_container')
+  }
 }
 
 let btn = document.getElementById('btn_menu')
-let main_card = document.getElementById('section_main')
-let btn_close = document.querySelector('.btn_close')
-
+let btn_close = document.querySelector('#section_burguer')
+let hamburguesa = document.querySelector('.menu_hamburguesa')
+let section_main = document.querySelector('#section_main')
 if (btn) {
   btn.addEventListener('click', open)
 }
